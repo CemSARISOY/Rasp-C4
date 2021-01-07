5b311706f6798e8436cf6970230b052fa9889409
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-01-07 16:04:22 +0100

    button

--- Puissance4/Player.py
+++ Puissance4/Player.py
@@ -5,6 +5,7 @@ import json
 import time
 import io
 
+from lib_grovepi.button import *
 
 class Player:
     def __init__(self, id, commandline=False):
@@ -22,32 +23,39 @@ class Player:
             Retourne la colonne entrée dans le terminal si le joueur est en mode terminal,
             sinon retourne la colonne detectée grâce à la photo prise.
         """
+        button = 2
+        initButton(button)
         print("Joueur " + str(self.__id))
         if(not self.__isCommandline):
-            differenceTab = []
-            col = -1
-            while(col == -1):
-                # os.system("sudo raspistill -o plateau.png")
-                print(os.system("python3 ../scripts/detection.py plateau"))
-
-                while(not os.path.exists("tab.txt")):  # Bloquer
+            try:
+                while (readButton(button) == 0):
                     time.sleep(0.1)
-                with io.open("tab.txt", mode="r", encoding="utf8") as f:
-                    text = f.read()
+                differenceTab = []
+                col = -1
+                while(col == -1):
+                    # os.system("sudo raspistill -o plateau.png")
+                    print(os.system("python3 ../scripts/detection.py plateau"))
 
-                tab = json.loads(text)  # parse les données du fichier tab.txt
-                os.remove("tab.txt")
+                    while(not os.path.exists("tab.txt")):  # Bloquer
+                        time.sleep(0.1)
+                    with io.open("tab.txt", mode="r", encoding="utf8") as f:
+                        text = f.read()
 
-                # fonction de detection de différence
+                    tab = json.loads(text)  # parse les données du fichier tab.txt
+                    os.remove("tab.txt")
 
-                difference = board.compare(tab)
+                    # fonction de detection de différence
 
-                # Si il y a une unique différence entre les données detectées et le board en mémoire,
-                # alors le joueur à joué cette case
-                if(len(difference) == 1):
-                    col = difference[0][1]
-                else:
-                    time.sleep(2)
+                    difference = board.compare(tab)
+
+                    # Si il y a une unique différence entre les données detectées et le board en mémoire,
+                    # alors le joueur à joué cette case
+                    if(len(difference) == 1):
+                        col = difference[0][1]
+                    else:
+                        time.sleep(2)
+            except IOError:
+                print("error")
         else:
             col = int(input())
         return col
