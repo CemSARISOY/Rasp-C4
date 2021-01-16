68cf182b9a3c3b0bf7ea1d00f32a1648a3f4303b
CemSARISOY <47150584+CemSARISOY@users.noreply.github.com>
2021-01-16 19:27:47 +0100

    Delete Puissance4 directory

--- Puissance4/main.py
+++
@@ -1,8 +0,0 @@
-from Player import *
-from IAMinMax import *
-from Game import *
-
-# game = Game()  # Partie réelle contre IA
-game = Game(commandLine=True)  # Partie terminal contre IA
-# game = Game(commandLine=True, playerOnly=True) # Partie terminal contre Joueur
-game.run()
