19f25a322ac1c6261f0ee9e1a5b33029fcdf8fb8
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-01-19 18:09:21 +0100

    detection fix

--- scripts/detection.py
+++ scripts/detection.py
@@ -1,15 +1,16 @@
 import numpy as np
 import cv2 as cv
+import sys
 import io
 
 boundaries = [
     ([0, 15, 100], [50, 50, 255]),
     ([0, 150, 150], [50, 255, 255]),
 ]
-imgG = cv.imread('plateau.png', 0)
+imgG = cv.imread(str(sys.argv[1]) + '.png', 0)
 cimg = cv.cvtColor(imgG, cv.COLOR_GRAY2BGR)
 
-img = cv.imread("plateau.png", 1)
+img = cv.imread(str(sys.argv[1]) + ".png", 1)
 
 lowerRed = np.array(boundaries[0][0], dtype="uint8")
 upperRed = np.array(boundaries[0][1], dtype="uint8")
@@ -34,35 +35,39 @@ for i in circles[0, :]:
     cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
 
-circlesRed = np.uint16(np.around(circlesRed))
 tabRed = []
-for i in circlesRed[0, :]:
-    x = i[0]
-    y = i[1]
-    l = len(gimg[0])/7
-    h = len(gimg)/6
-    colonne = x // l
-    hauteur = y // h
-    tabRed.append((int(colonne), int(hauteur)))
+if(not (circlesRed is None)):
+    circlesRed = np.uint16(np.around(circlesRed))
+    for i in circlesRed[0, :]:
+        x = i[0]
+        y = i[1]
+        l = len(gimg[0])/7
+        h = len(gimg)/6
+        colonne = x // l
+        hauteur = y // h
+        tabRed.append((int(colonne), int(hauteur)))
+
 outputYellow = cv.bitwise_and(img, img, mask=maskYellow)
 gimg = cv.cvtColor(outputYellow, cv.COLOR_BGR2GRAY)
 circlesYellow = cv.HoughCircles(gimg, cv.HOUGH_GRADIENT, 1, 150,
                                 param1=1, param2=20, minRadius=60, maxRadius=120)
-circles = np.uint16(np.around(circlesYellow))
-for i in circles[0, :]:
-    # draw the outer circle
-    cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
 tabYellow = []
-for i in circlesYellow[0, :]:
-    x = i[0]
-    y = i[1]
-    l = len(gimg[0])/7
-    h = len(gimg)/6
-    colonne = x // l
-    hauteur = y // h
-    tabYellow.append((int(colonne), int(hauteur)))
+if(not (circlesYellow is None)):
+    circles = np.uint16(np.around(circlesYellow))
+    for i in circles[0, :]:
+        # draw the outer circle
+        cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+    for i in circlesYellow[0, :]:
+        x = i[0]
+        y = i[1]
+        l = len(gimg[0])/7
+        h = len(gimg)/6
+        colonne = x // l
+        hauteur = y // h
+        tabYellow.append((int(colonne), int(hauteur)))
 # print(tabRed)  # Red = 1
 # print(tabYellow)  # Yellow =2
 
