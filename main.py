ac647b1c09265680293187c8acb80e9f0e1af0a4
Cem SARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-01-19 17:19:30 +0100

    catch

--- main.py
+++ main.py
@@ -13,59 +13,62 @@ button = 2
 initButton(button)
 
 score = [0, 0]
-setText("Veuillez appuyer\npour commencer")
-while(readButton(button) == 0):
-    time.sleep(0.1)
+try:
+    setText("Veuillez\nappuyer")
+    while(readButton(button) == 0):
+        time.sleep(0.1)
 
-while(True):
+    while(True):
 
-    setText("choisir la\ndifficulte")
-    choisi = False
-    alreadyPressed = False
-    pressedOnce = False
+        setText("choisir la\ndifficulte")
+        choisi = False
+        alreadyPressed = False
+        pressedOnce = False
 
-    # -- Choix de la difficulté
-    while(not choisi):
-        while(readButton(button) == 0):
-            waitedTime = time.time()
-            try:
-                if(round(waitedTime - pressedTime, 2) > 0.3):
-                    pressedOnce = True
-                    alreadyPressed = False
-                    i = i + 1
-                    if(i == 3):
-                        i = 0
-                    diffString = "Difficulte ", (i+1)
-                    setText(diffString)
-            except:
-                pass
-            time.sleep(0.05)
-        while(readButton(button) == 1):
-            time.sleep(0.05)
-        pressedTime = time.time()
+        # -- Choix de la difficulté
+        while(not choisi):
+            while(readButton(button) == 0):
+                waitedTime = time.time()
+                try:
+                    if(round(waitedTime - pressedTime, 2) > 0.3):
+                        pressedOnce = True
+                        alreadyPressed = False
+                        i = i + 1
+                        if(i == 3):
+                            i = 0
+                        diffString = "Difficulte ", (i+1)
+                        setText(diffString)
+                except:
+                    pass
+                time.sleep(0.05)
+            while(readButton(button) == 1):
+                time.sleep(0.05)
+            pressedTime = time.time()
 
-        if(alreadyPressed and pressedOnce):
-            choisi = True
-        else:
-            pressedOnce = True
-    # -- Fin choix de difficulté
+            if(alreadyPressed and pressedOnce):
+                choisi = True
+            else:
+                pressedOnce = True
+        # -- Fin choix de difficulté
 
-    if(oldDifficulty != -1 and oldDifficulty != tabDifficulty[i]):
-        setText("Réinitialisation\ndes scores")
-        score = [0, 0]
+        if(oldDifficulty != -1 and oldDifficulty != tabDifficulty[i]):
+            setText("Réinitialisation\ndes scores")
+            score = [0, 0]
+            time.sleep(1)
+
+        oldDifficulty = tabDifficulty[i]
+
+        setText("La partie va\ncommencer !")
         time.sleep(1)
 
-    oldDifficulty = tabDifficulty[i]
+        game = Game(iaDepth=tabDifficulty[i])  # Partie réelle contre IA
+        # game = Game(commandLine=True)  # Partie terminal contre IA
+        # game = Game(commandLine=True, playerOnly=True)  # Partie terminal contre Joueur
+        winner = game.run()
+        score[winner - 1] = score[winner - 1] + 1
 
-    setText("La partie va\ncommencer !")
-    time.sleep(1)
-
-    game = Game(iaDepth=tabDifficulty[i])  # Partie réelle contre IA
-    # game = Game(commandLine=True)  # Partie terminal contre IA
-    # game = Game(commandLine=True, playerOnly=True)  # Partie terminal contre Joueur
-    winner = game.run()
-    score[winner - 1] = score[winner - 1] + 1
-
-    scoreString = "Vous ", score[0], " - ", score[1], " Ennemi"
-    setText(scoreString)
-    time.sleep(1)
+        scoreString = "Vous ", score[0], " - ", score[1], " Ennemi"
+        setText(scoreString)
+        time.sleep(1)
+except IOError:
+    print("error")
